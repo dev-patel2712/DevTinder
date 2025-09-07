1e51e7234fd74b2b10dfa4fd60fdfa3f914e74e4
DEV PATEL <hiraparadev.dh@gmail.com>
2025-09-07 22:24:17 +0530

    latest work pushed

--- homework.md
+++ homework.md
@@ -14,4 +14,7 @@
 -.gitignore
 -create a remote repo on github
 -push all code to remote origin
+- Push all code to remote origin
+- Play with routes and route extensions ex. /hello, / , hello/2, /xyz
+- Order of the routes matter a lot
 
--- src/app.js
+++ src/app.js
@@ -2,12 +2,17 @@ const express = require("express");
 
 const app = express();
 
+
 app.use("/test",(req,res)=>{
     res.send("hii this is test");
 })
 
 app.use("/hello",(req,res)=>{
     res.send("hii this is hello");
+})
+
+app.use("/",(req,res)=>{
+    res.send("Hello, Dev");
 })
 
 app.listen("7777");
