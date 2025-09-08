06da9373048ab854fb3675fb000e606e745e98d3
dev patel <devp.envitics@gmail.com>
2025-09-08 12:12:26 +0530

    playin with routes and reading query params in routes

--- src/app.js
+++ src/app.js
@@ -2,24 +2,14 @@ const express = require("express");
 
 const app = express();
 
+// app.get("/users/:userId/:name/:password",(req,res)=>{
+//     console.log(req.params);
+//     res.send({firstName:"Dev", lastName:"Patel"});
+// })
+
 app.get("/users",(req,res)=>{
+    console.log(req.query);
     res.send({firstName:"Dev", lastName:"Patel"});
-})
-
-app.post("/users",(req,res)=>{
-    res.send("Data saved to db successfully");
-})
-
-app.patch("/users",(req,res)=>{
-    res.send("Data updated successfully");
-})
-
-app.delete("/users",(req,res)=>{
-    res.send("Data deleted successfully");
-}
-)
-app.use("/test",(req,res)=>{
-    res.send("hello from the server");
 })
 
 app.listen("7777");
