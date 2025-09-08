7ce68764385c0ca4fe7232336ec4aba5f92f477f
dev patel <devp.envitics@gmail.com>
2025-09-08 11:17:22 +0530

    explored get,post,patch,delete

--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ const express = require("express");
 
 const app = express();
 
+app.get("/users",(req,res)=>{
+    res.send({firstName:"Dev", lastName:"Patel"});
+})
 
+app.post("/users",(req,res)=>{
+    res.send("Data saved to db successfully");
+})
+
+app.patch("/users",(req,res)=>{
+    res.send("Data updated successfully");
+})
+
+app.delete("/users",(req,res)=>{
+    res.send("Data deleted successfully");
+}
+)
 app.use("/test",(req,res)=>{
-    res.send("hii this is test");
-})
-
-app.use("/hello",(req,res)=>{
-    res.send("hii this is hello");
-})
-
-app.use("/",(req,res)=>{
-    res.send("Hello, Dev");
+    res.send("hello from the server");
 })
 
 app.listen("7777");
