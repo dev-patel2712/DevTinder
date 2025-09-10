8597d1aac2296067145da8a3af1071e7cdf328d5
dev patel <devp.envitics@gmail.com>
2025-09-10 11:43:50 +0530

    delete-user API

--- src/app.js
+++ src/app.js
@@ -7,34 +7,34 @@ const User = require("./models/user")
 app.use(express.json());
 
 //signup route to create a new user
-app.post('/signup', async(req,res)=>{
-    
+app.post('/signup', async (req, res) => {
+
     const user = new User(req.body)
-    try{
+    try {
         await user.save();
         res.send("User added successfully");
     }
-    catch(error){
-        res.status(400).send("Error saving the user:",error.message);
+    catch (error) {
+        res.status(400).send("Error saving the user:", error.message);
     }
 
 })
 
 //get all users by emailId 
 
-app.get('/users', async(req,res)=>{
-    
+app.get('/users', async (req, res) => {
+
     const userEmail = req.body.emailId;
-       try{
-        const users = await User.find({emailId:userEmail});
-        if(users.length===0){
+    try {
+        const users = await User.find({ emailId: userEmail });
+        if (users.length === 0) {
             return res.status(404).send("No user found with this emailId");
         }
-        else{
+        else {
             res.send(users);
         }
     }
-    catch(error){
+    catch (error) {
         res.status(400).send("Something went wrong !");
     }
 
@@ -42,18 +42,18 @@ app.get('/users', async(req,res)=>{
 
 //Feed route to get all users
 
-app.get('/feed', async(req,res)=>{
-    
-       try{
+app.get('/feed', async (req, res) => {
+
+    try {
         const users = await User.find({});
-        if(users.length===0){
+        if (users.length === 0) {
             return res.status(404).send("No user found");
         }
-        else{
+        else {
             res.send(users);
         }
     }
-    catch(error){
+    catch (error) {
         res.status(400).send("Something went wrong !");
     }
 
@@ -62,18 +62,18 @@ app.get('/feed', async(req,res)=>{
 
 //get only one user by emailId with findOne method (even if two users have same emailId it will return only one user)
 
-app.get('/user', async(req,res)=>{
+app.get('/user', async (req, res) => {
     const userEmail = req.body.emailId;
-       try{
-        const user = await User.findOne({emailId:userEmail});
-        if(!user){
+    try {
+        const user = await User.findOne({ emailId: userEmail });
+        if (!user) {
             return res.status(404).send("No user found with this emailId");
         }
-        else{
+        else {
             res.send(user);
         }
     }
-    catch(error){
+    catch (error) {
         res.status(400).send("Something went wrong !");
     }
 })
@@ -81,28 +81,39 @@ app.get('/user', async(req,res)=>{
 
 //findbyId method to get user by _id
 
-app.get('/userById', async(req,res)=>{
+app.get('/userById', async (req, res) => {
     const id = req.body.id;
-       try{
+    try {
         const user = await User.findById(id);
-        if(!user){
+        if (!user) {
             return res.status(404).send("No user found with this Id");
         }
-        else{
+        else {
             res.send(user);
         }
     }
-    catch(error){
+    catch (error) {
         res.status(400).send("Something went wrong !");
     }
 })
 
+//delete user from database by id
+app.delete('/user', async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        await User.findByIdAndDelete(userId);
+        return res.send("User deleted successfully");
+    }
+    catch (error) {
+        res.status(400).send("Something went wrong !");
+    }
+})
 
-connectDB().then(()=>{
-console.log("DB connected successfully");
-app.listen("7777",()=>{
-    console.log("Server is succesfully listening on port 7777")
-});
-}).catch((err)=>{
-    console.error("DB not connected",err);
+connectDB().then(() => {
+    console.log("DB connected successfully");
+    app.listen("7777", () => {
+        console.log("Server is succesfully listening on port 7777")
+    });
+}).catch((err) => {
+    console.error("DB not connected", err);
 })
